a2e9762c5f4a2ae07fdebf4dd23f80b9cf735a26
disVill <konokou2000@gmail.com>
2022-10-06 03:12:59 +0900

    wip

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
