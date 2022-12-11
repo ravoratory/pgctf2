2a180ed35ddd50f5401cb2d4389f731910dba314
disVill <konokou2000@gmail.com>
2022-12-12 03:17:39 +0900

    Logout追加

--- server/pgrit/urls.py
+++ server/pgrit/urls.py
@@ -1,7 +1,10 @@
+from dj_rest_auth.views import LogoutView
+
 from django.urls import path
 
 from .views import PGritLoginView
 
 urlpatterns = [
     path("login", PGritLoginView.as_view(), name="login"),
+    path("logout", LogoutView.as_view(), name="logout"),
 ]
