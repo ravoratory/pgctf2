5850d21bd96a545d05506a41cc9bb960c2d81e17
disVill <konokou2000@gmail.com>
2022-12-11 17:54:10 +0900

    rm unused file

--- server/game_configurations/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "game_configurations"
-urlpatterns = []
--- server/game_configurations/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import status
-from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-
-# Create your views here.
