dda8276f551136cc9a9a4e51f02095499e0740fb
Ravie403 <info@tkm.blue>
2023-01-04 06:19:11 +0900

    add origin whitelist

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -9,6 +9,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", cast=Csv())
+CORS_ORIGIN_WHITELIST = config("CORS_ORIGIN_WHITELIST", cast=Csv())
 
 MIDDLEWARE += [  # noqa: F405
     "whitenoise.middleware.WhiteNoiseMiddleware",
