27a491867bba0a519156f9bd25ad4ea85b9f5771
disVill <konokou2000@gmail.com>
2023-01-07 23:49:23 +0900

    ローカルでログインする用エンドポイント追加

--- server/config/urls/development.py
+++ server/config/urls/development.py
@@ -1,4 +1,6 @@
 import debug_toolbar
+from allauth.socialaccount.providers.oauth2.urls import default_urlpatterns
+from pgrit.provider import PGritProvider
 
 from django.urls import include, path
 
@@ -7,5 +9,9 @@ from .urls import urlpatterns
 # fmt: off
 urlpatterns += [
     path("__debug__/", include(debug_toolbar.urls)),
+    # 手動でOAuth Tokenを入手してPOSTしなくてもログインできるようにする
+    # login: /pgrit/login/
+    # callback: /pgrit/login/callback/
+    *default_urlpatterns(PGritProvider),
 ]
 # fmt: on
