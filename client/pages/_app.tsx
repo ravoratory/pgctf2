dbb94cf33294014c8f59b60918bcc645d3597fd8
Ravie403 <info@tkm.blue>
2023-02-13 14:27:27 +0900

    add analytics

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,11 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { NextUIProvider } from '@nextui-org/react'
+import { Analytics } from '@vercel/analytics/react'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <NextUIProvider>
         <Component {...pageProps} />
+        <Analytics />
       </NextUIProvider>
     </SessionProvider>
   )
